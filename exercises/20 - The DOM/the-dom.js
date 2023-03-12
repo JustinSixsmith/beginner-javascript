9c2d371e01ce6b7b5b84f1bf5db020c15ff3f644
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-11 21:15:22 -0600

    selects h2 and replaces with new text

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,9 @@
+const p = document.querySelector("p");
+const divs = document.querySelectorAll("p");
+
+const heading = document.querySelector("h2");
+console.log(heading.textContent);
+console.log(heading.innerHTML);
+// Set the text content property on the h2 element
+// heading.textContent = "Justin is cool";
+// console.log(heading.textContent);
--- playground/some.js
+++ playground/some.js
@@ -1,11 +1,24 @@
-const add = (a, b = 3) => a + b;
+// const add = (a, b = 3) => a + b;
 
-function sayHi() {
-  console.log(`Hey, Justin!`);
+// function sayHi() {
+//   console.log(`Hey, Justin!`);
+// }
+
+// const button = document.querySelector(".clickMe");
+
+// button.addEventListener("click", sayHi);
+
+// setTimeout(() => console.log("Done! Time to eat."), 10000);
+
+function createGreeting(greeting = "") {
+  const myGreet = greeting.toUpperCase();
+  return function (name) {
+    return `${myGreet} ${name}`;
+  };
 }
 
-const button = document.querySelector(".clickMe");
+const sayHello = createGreeting("Hello");
+const sayHey = createGreeting("Hey");
 
-button.addEventListener("click", sayHi);
-
-setTimeout(() => console.log("Done! Time to eat."), 10000);
+console.log(sayHello("Justin"));
+console.log(sayHello("JoJo"));
