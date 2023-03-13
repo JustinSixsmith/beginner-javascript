102763a7bdab2154acf0a179f189133068e80b74
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-12 23:57:33 -0500

    Preps twosum

--- playground/some.js
+++ playground/some.js
@@ -10,15 +10,25 @@
 
 // setTimeout(() => console.log("Done! Time to eat."), 10000);
 
-function createGreeting(greeting = "") {
-  const myGreet = greeting.toUpperCase();
-  return function (name) {
-    return `${myGreet} ${name}`;
-  };
+// function createGreeting(greeting = "") {
+//   const myGreet = greeting.toUpperCase();
+//   return function (name) {
+//     return `${myGreet} ${name}`;
+//   };
+// }
+
+// const sayHello = createGreeting("Hello");
+// const sayHey = createGreeting("Hey");
+
+// console.log(sayHello("Justin"));
+// console.log(sayHello("JoJo"));
+
+function twoSum(nums, target) {
+  for (let i = 0; i < num.length; i++) {
+    for (let j = 0; j < num.length; j++) {
+      if (i + j === target) return [i, j];
+    }
+  }
 }
 
-const sayHello = createGreeting("Hello");
-const sayHey = createGreeting("Hey");
-
-console.log(sayHello("Justin"));
-console.log(sayHello("JoJo"));
+console.log([2, 7, 11, 15], 9);
