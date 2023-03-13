05bc9d9100d762f7af9524bced6f7ad010412278
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-03-13 00:02:46 -0500

    Solves for twoSum

--- playground/some.js
+++ playground/some.js
@@ -24,11 +24,12 @@
 // console.log(sayHello("JoJo"));
 
 function twoSum(nums, target) {
-  for (let i = 0; i < num.length; i++) {
-    for (let j = 0; j < num.length; j++) {
-      if (i + j === target) return [i, j];
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) return [i, j];
     }
   }
 }
 
-console.log([2, 7, 11, 15], 9);
+const nums = [2, 7, 11, 15];
+console.log(twoSum(nums, 9));
